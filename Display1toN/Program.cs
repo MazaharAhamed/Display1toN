673661737e8c30ddd79851f58db769da540d601c
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-17 19:36:46 +0530

    Add project files.

---
+++ Display1toN/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Display1toN
+{
+    class Program
+    {
+        public static void Print1(int n)
+        {
+            if (n == 0)
+                return;
+            Console.Write(n+" ");
+            Print1(n - 1);
+        }
+
+        public static void Print2(int n)
+        {
+            if (n == 0)
+                return;
+            Print2(n - 1);
+            Console.Write(n + " ");
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Print1(x);
+            Console.WriteLine("");
+
+            Print2(x);
+            Console.WriteLine("");
+        }
+    }
+}
